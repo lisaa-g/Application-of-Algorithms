06c51e0a39590196810ab65166a775cba37c2224
lisaag <lisa.godwin2002@gmail.com>
2024-10-17 19:39:54 +0200

    add: csv files and plot

--- Assignment 3/plots.py
+++ Assignment 3/plots.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 # Load the CSV file generated from the C++ code
 file_path = "Assignment 3/partA1.csv"
 df = pd.read_csv(file_path)
+file_path2 = "Assignment 3/partA2.csv"
+df2 = pd.read_csv(file_path2)
 
 # Plotting the results
 plt.figure(figsize=(10, 6))
 plt.plot(df['Number of Sets'], df['Time (microseconds)'], color='magenta', label='Linked List Weighted-Union')
+plt.plot(df2['Number of Sets'], df2['Time (microseconds)'], color='skyblue', label='Union-by-Rank and Path Compression')
 
 # Adding labels and title
 plt.xlabel('Number of Sets (n)')
-plt.ylabel('Time (seconds)')
-plt.title('Time Complexity of Disjoint-Set Operations with Linked List (Weighted-Union)')
+plt.ylabel('Average Time (microseconds)')
+plt.title('Time Complexity of Disjoint-Set Operations')
 plt.legend()
 plt.grid(True)
 
