4779ecf06a4c609e6250d5b43b9a19251f35257b
lisaag <lisa.godwin2002@gmail.com>
2024-10-22 13:55:01 +0200

    print set

--- Assignment 3/disjoint_set_forest.cpp
+++ Assignment 3/disjoint_set_forest.cpp
@@ -11,6 +11,7 @@ struct Node {
     int data;
     Node* parent;
     int rank;
+    Node* next; //pointer to the next member in the set
 };
 
 //create a set
@@ -19,6 +20,7 @@ Node* makeSet(int x) {
     node->data = x;
     node->parent = node; //node is its own parent
     node->rank = 0; //rank is 0 when the set is created
+    node->next = node;
     return node;
 }
 
@@ -34,8 +36,12 @@ Node* findSet(Node* x) {
 void link(Node* x, Node* y) {
     if (x->rank > y->rank) {
         y->parent = x; //add ys tree to x
+        y->next = x->next; // Link y's next to x's next
+        x->next = y; 
     } else {
         x->parent = y; //add xs tree to y
+        x->next = y->next; // Link x's next to y's next
+        y->next = x;
         if (x->rank == y->rank) {
             y->rank++; //increment y rank if the ranks are equal
         }
@@ -47,10 +53,18 @@ void unionSets(Node* x, Node* y) {
     link(findSet(x), findSet(y));
 }
 
-//print the represenattive of the set that x belongs to
-void printSetRepresentative(Node* x) {
-    Node* rep = findSet(x);
-    cout << "Representative of " << x->data << " is " << rep->data << endl;
+//print the members of the set containing node x
+void printSet(Node* x) {
+    Node* y = findSet(x);
+    std::cout << "Members of the set: ";
+    std::cout << y->data << " ";
+
+    Node* z = y->next;
+    while (z != y) { 
+        std::cout << z->data << " ";
+        z = z->next;
+    }
+    std::cout << std::endl;
 }
 
 // Function to record time for union and find operations
@@ -82,34 +96,34 @@ void recordTime(int size, ofstream& csv_file) {
 }
 
 int main() {
-    //creates sets
+    // Creates sets
     Node* node1 = makeSet(1);
     Node* node2 = makeSet(2);
     Node* node3 = makeSet(3);
     Node* node4 = makeSet(4);
 
     cout << "Initial representatives:" << endl;
-    printSetRepresentative(node1);
-    printSetRepresentative(node2);
-    printSetRepresentative(node3);
-    printSetRepresentative(node4);
-    
+    printSet(node1);
+    printSet(node2);
+    printSet(node3);
+    printSet(node4);
+
     cout << "\nUnion of set 1 and set 2:" << endl;
     unionSets(node1, node2);
-    printSetRepresentative(node1);
-    printSetRepresentative(node2);
-    
-    cout << "\nUnion of set 3 and set 4:" << endl;
-    unionSets(node3, node4);
-    printSetRepresentative(node3); 
-    printSetRepresentative(node4);
-    
-    cout << "\nUnion of set 1+2 and set 3+4:" << endl;
-    unionSets(node1, node3);
-    printSetRepresentative(node1);
-    printSetRepresentative(node2);
-    printSetRepresentative(node3);
-    printSetRepresentative(node4);
+    printSet(node1);
+    printSet(node2);
+
+    cout << "\nUnion of set 1+2 and set 3:" << endl;
+    unionSets(node2, node3);
+    printSet(node2); 
+    printSet(node3);
+
+    cout << "\nUnion of set 1+2+3 and set 4:" << endl;
+    unionSets(node1, node4);
+    printSet(node1);
+    printSet(node2);
+    printSet(node3);
+    printSet(node4);
 
     int sizes[] = {100, 1000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
 
